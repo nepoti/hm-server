7bd23ab3529640bc4a06815e80442ab43fd76f1d
Oleg Gromyak <o.a.gromyak@gmail.com>
2015-07-09 13:55:23 +0300

    Created UserProfile model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    name = models.CharField(max_length=30)
+    profile_image = models.URLField(blank=True, default=u'')
+    gender = models.CharField(blank=True, default=u'', max_length=20)
+    country = models.CharField(blank=True, default=u'', max_length=50)
+    city = models.CharField(blank=True, default=u'', max_length=200)
+    birthday = models.DateField(blank=True, null=True)
+    about = models.CharField(blank=True, default=u'',  max_length=100)
+    achievements = models.TextField(default=u'{}')
